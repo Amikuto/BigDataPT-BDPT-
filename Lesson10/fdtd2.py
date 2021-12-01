53699f010f119c01461ca525cbe5420e36c4778b
Damir Kurtaev <damir.kurtaev@yandex.ru>
2021-12-01 23:33:55 +0300

    Task 10: 5 done

--- Lesson10/fdtd2.py
+++ Lesson10/fdtd2.py
@@ -1,8 +1,9 @@
+import multiprocessing
+
 from avgNumOfSteps import avg_num_of_steps
 import pandas as pd
 
 
-def from_dict_to_dfs2(file_name, output):
-	# return output.put(pd.DataFrame.from_dict(avg_num_of_steps("out/" + file_name), orient='index'))
-	return output.put(set(avg_num_of_steps("out/" + file_name)))
-
+def from_dict_to_dfs2(file_name: multiprocessing.Queue, output: multiprocessing.Queue) -> None:
+    for files in iter(file_name.get, None):
+        output.put(pd.DataFrame.from_dict(avg_num_of_steps("out/" + files), orient='index'))
